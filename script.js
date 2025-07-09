9fa00f6d04d6e0663fa0a7ce3c5c2181162ea61f
Kallebe <kallebe.reis.soares@gmail.com>
2025-07-09 13:51:14 -0300

    Added logic

---
+++ script.js
@@ -0,0 +1,16 @@
+const form = document.getElementById('form');
+form.addEventListener('submit', handleSubmit)
+
+const inputValue = document.getElementById('value_real');
+const selectedCurrency = document.getElementById('currency');
+const result = document.getElementById('result');
+
+function handleSubmit(e) {
+  e.preventDefault();
+  if(!inputValue.value || inputValue.value < 0) {
+    alert('Informe um valor correto!');
+    return;
+  } else if (selectedCurrency.value) {
+    alert('Escolha uma moeda!')
+  }
+};
