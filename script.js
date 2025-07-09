675000c2eae669f306a70f33a8ed6114cc63d348
Kallebe <kallebe.reis.soares@gmail.com>
2025-07-09 15:26:57 -0300

    Added animation

--- script.js
+++ script.js
@@ -26,10 +26,13 @@ function converter() {
   if (selectedCurrency.value === 'eur') {
     valueConverted = inputValue.value / 6.42;
     result.innerHTML = valueFormatter('pt-BR', 'EUR');
+    animationResult();
 
   } else if (selectedCurrency.value === 'dol') {
     valueConverted = inputValue.value / 5.48;
     result.innerHTML = valueFormatter('en-US', 'USD');
+    animationResult();
+    
   }
 
   inputValue.value = '';
@@ -39,4 +42,11 @@ function converter() {
 function valueFormatter(locale, currency) {
   const value = valueConverted.toLocaleString(locale, { style: 'currency', currency: currency });
   return `<span>${value}</span>`;
+}
+
+function animationResult() {
+  return result.animate([
+    {transform: 'translateY(-150px)'},
+    {transform: 'translateY(0px)'},
+  ], {duration: 500});
 }
